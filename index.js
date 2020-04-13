5e456062f3ca978a295771554436cc402b976a92
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-04-13 20:45:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ function happyHolidaysTo(name) {
 }
 
 function happyCustomHolidayTo(Easter, name) {
-  var holiday = "Easter"
-  var name = "Sal"
-  returns (`Happy ${holiday}, ${Sal}`)
+  var holiday = "Easter";
+  var name = "Sal";
+  returns (`Happy ${holiday}, ${Sal}`);
 }
 
 function holidayCountdown(Easter, 5) {
-  var holiday = "Easter"
-  var days = "5"
-  returns (`It\'s ${days} days until ${holiday}!`) 
+  var holiday = "Easter";
+  var days = "5";
+  returns (`It\'s ${days} days until ${holiday}!`); 
 }
