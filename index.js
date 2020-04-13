73f785747525b3fc99e85398aee3d7a284f98962
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-04-13 20:05:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ function happyHolidaysTo(name) {
  var name = "Sal"
  console.log(`Happy holidays, ${name}`)
 }
+
+function happyCustomHolidayTo(Easter, Sal) {
+  var holiday = "Easter"
+  var name = "Sal"
+  console.log(`Happy ${holiday}, ${Sal}`)
+}
